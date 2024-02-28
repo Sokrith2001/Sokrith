489ade0cf0f713d6759f26c695941f7e59de14a4
Sokrith2001 <pensokrith2001@gmail.com>
2024-02-28 17:41:28 +0700

    first commit

---
+++ app/src/main/java/com/rith/ctityapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.rith.ctityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
